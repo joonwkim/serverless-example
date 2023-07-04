103b219583f040054b14b58780732d558acca080
Joonwan Kim <joonwk@gmail.com>
2023-07-04 21:56:29 +0900

    recovered

--- services/todoService.ts
+++ services/todoService.ts
@@ -1,9 +1,7 @@
-// import prisma from '@/lib/prisma'
-import { PrismaClient } from '@prisma/client'
+import prisma from '@/lib/prisma'
 
 export async function getTodos() {
     try {
-      const  prisma = new PrismaClient()
         const todos = await prisma.todo.findMany()
         return {todos}
     } catch (error) {
@@ -11,22 +9,22 @@ export async function getTodos() {
     }
 }
 
-// export async function createTodo(title:string){
-//     try{
-//         const todo = await prisma.todo.create({data:{title}})
-//         return {todo}
-//     }
-//     catch(error){
-//         return({error})
-//     }
-// }
+export async function createTodo(title:string){
+    try{
+        const todo = await prisma.todo.create({data:{title}})
+        return {todo}
+    }
+    catch(error){
+        return({error})
+    }
+}
 
-// export async function updateTodo(id: string, isCompleted:boolean) {
-//     try {
-//         const todo = await prisma.todo.update({where: {id},data:{isCompleted}})
-//         return {todo}
-//     } catch (error) {
-//         return({error})
-//     }
+export async function updateTodo(id: string, isCompleted:boolean) {
+    try {
+        const todo = await prisma.todo.update({where: {id},data:{isCompleted}})
+        return {todo}
+    } catch (error) {
+        return({error})
+    }
     
-// }
+}
