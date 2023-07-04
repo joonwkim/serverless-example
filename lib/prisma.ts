8290a9897bd80edbcca5b28b955c75e2ffc96f00
Joonwan Kim <joonwk@gmail.com>
2023-07-04 21:45:11 +0900

    prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,10 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-declare global{
-   var prisma: PrismaClient | undefined
-}
-const prisma = global.prisma || new PrismaClient()
+// declare global{
+//    var prisma: PrismaClient | undefined
+// }
+// const prisma = global.prisma || new PrismaClient()
 
-if(process.env.NODE_ENV === 'development') global.prisma = prisma
+// if(process.env.NODE_ENV === 'development') global.prisma = prisma
 
-export default prisma
+// export default prisma
