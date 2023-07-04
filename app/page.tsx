2dc5536b88562fbe87afdb4dacb96f859d8ad2cc
Joonwan Kim <joonwk@gmail.com>
2023-07-04 21:43:56 +0900

    prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
-// import { getTodos } from '@/services/todoService'
+import { getTodos } from '@/services/todoService'
 import TodoItem from './components/TodoItem'
 import NewTodo from './components/NewTodo'
 
 export default async function Home() {
-  // const { todos } = await getTodos()
+  const { todos } = await getTodos()
   return (
     <section className='py-20'>
       <div className="mt-3">
         <h1>Todos</h1>
         <NewTodo/>
         <h2 className='text-xl font-semibold mt-10 border-b pb-2'>Previous todos:</h2>
-        {/* <ul className='list-group'>
+        <ul className='list-group'>
           {todos?.map(todo => (
             <TodoItem key={todo.id} todo={todo} />
           ))}
-        </ul> */}
+        </ul>
       </div>
     </section>
   )
--- services/todoService.ts
+++ services/todoService.ts
@@ -1,7 +1,9 @@
-import prisma from '@/lib/prisma'
+// import prisma from '@/lib/prisma'
+import { PrismaClient } from '@prisma/client'
 
 export async function getTodos() {
     try {
+        prisma = new PrismaClient()
         const todos = await prisma.todo.findMany()
         return {todos}
     } catch (error) {
@@ -9,22 +11,22 @@ export async function getTodos() {
     }
 }
 
-export async function createTodo(title:string){
-    try{
-        const todo = await prisma.todo.create({data:{title}})
-        return {todo}
-    }
-    catch(error){
-        return({error})
-    }
-}
+// export async function createTodo(title:string){
+//     try{
+//         const todo = await prisma.todo.create({data:{title}})
+//         return {todo}
+//     }
+//     catch(error){
+//         return({error})
+//     }
+// }
 
-export async function updateTodo(id: string, isCompleted:boolean) {
-    try {
-        const todo = await prisma.todo.update({where: {id},data:{isCompleted}})
-        return {todo}
-    } catch (error) {
-        return({error})
-    }
+// export async function updateTodo(id: string, isCompleted:boolean) {
+//     try {
+//         const todo = await prisma.todo.update({where: {id},data:{isCompleted}})
+//         return {todo}
+//     } catch (error) {
+//         return({error})
+//     }
     
-}
+// }
