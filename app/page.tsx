86dcbf7d7466eb680ac192e4946fbfd71f162c75
Joonwan Kim <joonwk@gmail.com>
2023-07-04 20:49:50 +0900

    todo page added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { getTodos } from '@/services/todoService'
+import TodoItem from './components/TodoItem'
+import NewTodo from './components/NewTodo'
 
-export default function Home() {
+export default async function Home() {
+  const { todos } = await getTodos()
   return (
-    <h1>Home</h1>
+    <section className='py-20'>
+      <div className="mt-3">
+        <h1>Todos</h1>
+        <NewTodo/>
+        <h2 className='text-xl font-semibold mt-10 border-b pb-2'>Previous todos:</h2>
+        <ul className='list-group'>
+          {todos?.map(todo => (
+            <TodoItem key={todo.id} todo={todo} />
+          ))}
+        </ul>
+      </div>
+    </section>
   )
 }
